9680cddf32218b1c11a88fabd19a2d5e7ce11f93
Juanda9992 <juanda999games@gmail.com>
2022-06-24 18:29:57 -0500

    MAS UI y distancia del objetivo

--- SIMPLE JAM 2022/Assets/Scripts/Logic/Timer.cs
+++ SIMPLE JAM 2022/Assets/Scripts/Logic/Timer.cs
@@ -9,10 +9,14 @@ public class Timer : MonoBehaviour
     public delegate void onTimeOut();
     public static onTimeOut timeOut;
     public Slider slider;
+    public Image sliderImg;
     public int time;
+
     // Start is called before the first frame update
     void Start()
     {
+        sliderImg.DOColor(Color.red,time);
         slider.DOValue(0,time).OnComplete(()=>timeOut?.Invoke());
     }   
+    
 }
